ffc661ac8c4e13ccc208b5938eee983e40170ef6
Sourabh Mishra <59117474+Sourabhmishra-23@users.noreply.github.com>
2021-03-26 18:35:27 +0530

    update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -5,7 +5,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 const CACHE = "pwabuilder-page";
 
 const offlineFallbackPage = "index.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
